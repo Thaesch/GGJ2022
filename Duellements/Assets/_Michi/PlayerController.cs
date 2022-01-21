17dbc1464860eb14cc59529a2b5d9bdd4defa052
enemene28 <michi.enzmann@gmail.com>
2022-01-21 22:29:39 +0100

    added rotation to player

--- Duellements/Assets/_Michi/PlayerController.cs
+++ Duellements/Assets/_Michi/PlayerController.cs
@@ -17,7 +17,18 @@ public class PlayerController : MonoBehaviour
     {
         float hinput = Input.GetAxis("Horizontal");
         float vinput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(hinput, 0, vinput) * speed * Time.deltaTime);
-        
+        transform.Translate(new Vector3(hinput, 0, vinput) * speed * Time.deltaTime, Space.World);
+        Vector3 mousePos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            //Rotation anpassen.
+            Vector3 whereToLook = hit.point - transform.position;
+
+            transform.rotation = Quaternion.LookRotation(whereToLook, Vector3.up);
+        }
+            
     }
 }
