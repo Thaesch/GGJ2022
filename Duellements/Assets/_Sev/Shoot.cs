036c6ad8b0bc8082d852fe0c32638d493ebc182a
Sev <--replace-all>
2022-01-23 00:05:07 +0100

    only shoot own player

--- Duellements/Assets/_Sev/Shoot.cs
+++ Duellements/Assets/_Sev/Shoot.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,8 +15,11 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        PhotonView pv = GetComponent<PhotonView>();
+
         blockTime -= Time.deltaTime;
-        if (Input.GetMouseButton(0) && blockTime <= 0)
+        if (Input.GetMouseButton(0) && blockTime <= 0 && (!PhotonNetwork.IsConnected || pv.IsMine))
         {
             blockTime = minBlockTime;
             GameObject instance =  GameObject.Instantiate<GameObject>(projectile.gameObject, spawnPosition.position, spawnPosition.rotation);
