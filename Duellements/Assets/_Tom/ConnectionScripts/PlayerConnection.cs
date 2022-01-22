9a239fd0c0c5aba09aaa13ce4b08ee2ed1aeed68
Sev <--replace-all>
2022-01-22 22:12:17 +0100

    networking test

--- Duellements/Assets/_Tom/ConnectionScripts/PlayerConnection.cs
+++ Duellements/Assets/_Tom/ConnectionScripts/PlayerConnection.cs
@@ -9,11 +9,31 @@ public class PlayerConnection : MonoBehaviour
 
     [SerializeField] private Transform spawnPositions;
     [SerializeField] private FollowPlayer followPlayerCamera;
+    [SerializeField] private Transform SpawnerContainer;
+
 
     void Start()
     {
         if (!PhotonNetwork.IsConnected) { SceneManager.LoadScene("Connection"); followPlayerCamera.enabled = false; return; }
         followPlayerCamera.player = NetworkSpawner.Instantiate("Player", spawnPositions.GetChild(PhotonNetwork.CurrentRoom.PlayerCount - 1).position, Quaternion.identity).transform;
+
+
+        for (int i = 0; i < SpawnerContainer.childCount; i++)
+        {
+            GhostSpawn spawn = SpawnerContainer.GetChild(i).GetComponent<GhostSpawn>();
+            if (spawn)
+            {
+                if (PhotonNetwork.IsMasterClient)
+                {
+                    spawn.enabled = true;
+                }
+                else
+                {
+                    spawn.enabled = false;
+                }
+            }
+        }
+
     }
 
 }
