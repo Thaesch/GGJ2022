4b30f0edfd78304131cb1c8640cbbde8d6b098e1
mpiechot <marco.piechotta@gmail.com>
2022-01-22 18:40:05 +0100

    Update Guardian.cs

--- Duellements/Assets/_Sev/Guardian.cs
+++ Duellements/Assets/_Sev/Guardian.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using _Marco.Scripts;
 using UnityEngine;
 
 
@@ -7,12 +9,22 @@ using UnityEngine;
 [RequireComponent(typeof (Shoot))]
 public class Guardian : MonoBehaviour
 {
-
+    private Shoot weapon;
+    
     public Element element = Element.NORMAL;
+    public List<Projectile> projectiles = new List<Projectile>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        element = ElementManager.GetPlayerElement();
+        weapon = GetComponent<Shoot>();
+
+        Debug.Log($"projectiles: {projectiles.Count} elements: {(int)element-1}");
+        if (projectiles.Count > (int)element-1 && (int)element >= 0)
+        {
+            weapon.projectile = projectiles[(int)element-1];
+            Debug.Log("Set Projectile: " + projectiles[(int)element-1].name);
+        }
     }
 }
