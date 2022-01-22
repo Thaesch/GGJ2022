12b4c9ace7bb8ac69d3d1c473026adf15e4bd953
Sev <--replace-all>
2022-01-22 11:18:53 +0100

    Renamed Player

--- Duellements/Assets/_Sev/GuildIndicator.cs
+++ Duellements/Assets/_Sev/GuildIndicator.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class GuildIndicator : MonoBehaviour
 {
 
-    public Player player;
+    public Guardian player;
 
     // Start is called before the first frame update
     void Start()
